aba62ef2017a295a2b688425585aa23a9bf45e61
Elena <elena_tapean@yahoo.com>
2022-10-23 11:52:46 +0300

    modified product page and made responsive

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -3,29 +3,62 @@
         <router-link class="go-back" to="/"> 
            Go Back
         </router-link>
-        <h1>{{ product.name }}</h1>
-        <img class="product-img" :src="product.img" alt="book cover" />
-        <h2>Author:</h2>
-        <span>{{ product.author }}</span>
-        <h2>Genre:</h2>
-        <span>{{ product.collectionId }}</span>
-        <h2>Description:</h2>
-        <p v-html="product.description" />
-        <h2>Price:</h2>
-        <span>{{ product.price }} Lei</span>
-        <h2>Quantity:</h2>
-        <div>
-            <button class="quantity-button" @click="decrement(product.id)">-</button>
-            <span>{{ product.count || 0 }}</span>
-            <button class="quantity-button" :disabled="product.quantity && (product.quantity <= product.count)" 
-                  @click="increment(product.id)">+</button>
-            <br/>
-            <span v-if="product.quantity && (product.quantity <= product.count)">Max stock reached</span>
+        <div class="min-div">
+            <div class="shader-bg">
+                <h1>{{ product.name }}</h1>
+                <img class="product-img" :src="product.img" alt="book cover" />
+                <h2>Author:</h2>
+                <span>{{ product.author }}</span>
+                <h2>Genre:</h2>
+                <span>{{ product.collectionId }}</span>
+                <h2>Description:</h2>
+                <p v-html="product.description" />
+                <h2>Price:</h2>
+                <span>{{ product.price }} Lei</span>
+                <h2>Quantity:</h2>
+                <div>
+                    <button class="quantity-button" @click="decrement(product.id)">-</button>
+                    <span>{{ product.count || 0 }}</span>
+                    <button class="quantity-button" :disabled="product.quantity && (product.quantity <= product.count)" 
+                            @click="increment(product.id)">+</button>
+                    <span v-if="product.quantity && (product.quantity <= product.count)">Max stock reached</span>
+                </div>
+                <button class="basket-button" :disabled="!product.count || product.count === 0" 
+                        @click="addToBasket(product)">
+                    Add to basket
+                </button>
+            </div>
         </div>
-        <button class="basket-button" :disabled="!product.count || product.count === 0" 
-                @click="addToBasket(product)">
-            Add to basket
-        </button>
+        <div class="flex-div">
+            <div class="shader-bg">
+                <h1>{{ product.name }}</h1>
+                <h2>Author:</h2>
+                <span>{{ product.author }}</span>
+                <h2>Genre:</h2>
+                <span>{{ product.collectionId }}</span>
+                <h2>Description:</h2>
+                <p v-html="product.description" />
+                <div class="flex-div">
+                    <h2>Price:</h2>
+                    <span>{{ product.price }} Lei</span>
+                    <h2>Quantity:</h2>
+                    <div>
+                        <button class="quantity-button" @click="decrement(product.id)">-</button>
+                        <span>{{ product.count || 0 }}</span>
+                        <button class="quantity-button" :disabled="product.quantity && (product.quantity <= product.count)" 
+                                @click="increment(product.id)">+</button>
+                        <span v-if="product.quantity && (product.quantity <= product.count)">Max stock reached</span>
+                    </div>
+                    <button class="basket-button" :disabled="!product.count || product.count === 0" 
+                           @click="addToBasket(product)">
+                        Add to basket
+                    </button>
+                </div>
+            </div>
+            <div class="shader-bg">
+                <img class="product-img" :src="product.img" alt="book cover" />
+            </div>
+        </div>
     </main>
 </template>
 
@@ -73,6 +106,15 @@ export default {
         background-position: center;
         background-size: cover;
 
+        .shader-bg {
+            margin: 15px 25px;
+            padding: 3px;
+            background-color: rgba(255, 255, 255, 0.8);
+            border-radius: 5px;
+        }
+        .flex-div {
+            display: none;
+        }
         h1 {
             font-size: 28px;
             color: black;
@@ -97,7 +139,7 @@ export default {
         .go-back {
             margin: 20px;
             display: flex;
-            font-size: 17px;
+            font-size: 18px;
             font-style: italic;
             color: rgb(218, 30, 93);
             text-decoration: none;
@@ -141,35 +183,50 @@ export default {
     }
 
     @media only screen and (min-width: 1024px) {
+        .min-div {
+            display: none;
+        }
+        .flex-div {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+        }
+        .shader-bg {
+            margin: 15px 25px;
+            padding: 3px;
+            background-color: rgba(255, 255, 255, 0.8);
+            border-radius: 5px;
+        }
         h1 {
-            margin: 15px;
-            font-size: 30px;
+            margin: 10px;
+            font-size: 27px;
         }
         h2 {
             margin: 15px;
-            font-size: 21px;
-            color: black;
-            font-family: Arial, Helvetica, sans-serif;
+            font-size: 18px;
         }
         p {
-            margin: 0 50px;
+            margin: 0 3px;
+        }
+        span {
+            margin: 0 5px;
+            font-size: 17px;
         }
         .product-img {
-            margin: 0 50px;
-            width: 240px;
-            height: 320px;
-            float: right;
+            margin: 5px 8px;
+            width: 270px;
+            height: 420px;
             border-radius: 5px;
         }
         .go-back {
-            margin: 3px;
+            margin: 5px;
             margin-left: 50px;
             display: flex;
-            font-size: 19px;
+            font-size: 20px;
         }
         .quantity-button {
-            margin: 0 25px;
-            margin-bottom: 15px;
+            margin: 0 10px;
             padding: 5px 20px;
             border: none;
             border-radius: 4px;
@@ -181,9 +238,9 @@ export default {
             background-color: rgb(218, 30, 93);
         }
         .basket-button:disabled {
-            margin: 5px;
-            margin-bottom: 30px;
-            padding: 5px;
+            margin: 0;
+            margin-left: 30px;
+            padding: 7px;
             border: none;
             border-radius: 5px;
             background-color: rgba(128, 128, 128, 0.8);
@@ -193,9 +250,9 @@ export default {
             font-style: normal;
         }
         .basket-button:enabled {
-            margin: 5px;
-            padding: 8px;
-            margin-bottom: 30px;
+            margin: 0;
+            margin-left: 30px;
+            padding: 7px;
             border: none;
             border-radius: 5px;
             background-color: mediumseagreen;
